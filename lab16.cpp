4b3c3a1a647475df723f3de8f950284f15c6867a
daniela-kovatch <dkovatch@umich.edu>
2025-03-31 14:30:56 -0400

    Test 1

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,10 @@
 // Code to implement the gcf function goes here
+#include <iostream>
+using namespace std;
+
+// Recursive function to compute the GCF using the Euclidean Algorithm
+int gcf(int A, int B) {
+    if (B == 0)
+        return A;
+    return gcf(B, A % B);
+}
